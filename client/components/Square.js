a765e13ffccb3bf150546afa729b6aaca9cf7ae4
Yates Walker <yateswalker@gmail.com>
2018-02-02 10:38:44 -0600

    add coords to square js

--- client/components/Square.js
+++ client/components/Square.js
@@ -6,43 +6,54 @@ import { removeSelectedTile } from "../store/selectedTile";
 import { removeTileFromPouch, addTileToPouch } from "../store/playersPouch";
 
 export class Square extends Component {
-  constructor(props) {
-    super();
-    this.state = { tile: null };
-    this.clickHandler = this.clickHandler.bind(this);
-  }
+constructor(props) {
+super();
+this.state = { tile: null };
+this.clickHandler = this.clickHandler.bind(this);
+this.assignCoords = this.assignCoords.bind(this)
+}
 
-  clickHandler() {
-    if (!this.state.tile && this.props.selectedTile) {
-      this.setState({ tile: this.props.selectedTile });
-      this.props.removeTileFromPouch(this.props.selectedTile.id);
-      this.props.removeSelectedTile();
-    } else if (this.state.tile) {
-      this.props.addTileToPouch(this.state.tile);
-      this.setState({ tile: null });
-    } else {
-      console.log('weird things are happening')
-    }
-  }
+assignCoords(tile) {
+tile.x = this.props.children[1][0];
+tile.y = this.props.children[1][1];
+return tile;
+}
 
-  render() {
-    return (
-      <div
-        onClick={this.clickHandler}
-        style={{
-          width: "100%",
-          height: "100%"
-        }}
-      >
-        <div>
-          {this.state.tile && (
-            <img style={{ width: "100%" }} src={this.state.tile.img} />
-          )}
-        </div>
-        {this.props.children}
-      </div>
-    );
-  }
+clickHandler() {
+if (!this.state.tile && this.props.selectedTile) {
+const currentTile = this.props.selectedTile;
+const updatedTile = this.assignCoords(currentTile);
+this.setState({ tile: updatedTile });
+// firebase
+console.log('UPDATED TILE', updatedTile)
+this.props.removeTileFromPouch(this.props.selectedTile.id);
+this.props.removeSelectedTile();
+} else if (this.state.tile) {
+this.props.addTileToPouch(this.state.tile);
+this.setState({ tile: null });
+} else {
+console.log('weird things are happening')
+}
+}
+
+render() {
+return (
+<div
+onClick={this.clickHandler}
+style={{
+width: "100%",
+height: "100%"
+}}
+>
+<div>
+{this.state.tile && (
+<img style={{ width: "100%" }} src={this.state.tile.img} />
+)}
+</div>
+{this.props.children}
+</div>
+);
+}
 }
 
 const mapState = ({ selectedTile }) => ({ selectedTile });
--- client/components/WordSearch.js
+++ client/components/WordSearch.js
@@ -6,56 +6,65 @@ var board = [
   ['', 'D', '', '', '']
   ];
 
-// BANANA ISOLATES EACH INDIVIDUAL WORD BY IDENTIFYING CONTIGUOUS TILES AND ADDS THEM TO A JOINT ARRAY
-function banana(arr) {
+  // loop through rows
+  // loop through columns
+  // check if the image tag has an id or data-attribute that says the letter
+  // add to a string
+  // add that string to a list
+
+  // check if all the words in the list are actual wrods (firebase)
+  //
+
+  // BANANA ISOLATES EACH INDIVIDUAL WORD BY IDENTIFYING CONTIGUOUS TILES AND ADDS THEM TO A JOINT ARRAY
+  function banana(arr) {
   let horizWordArr = [];
   for (let row of arr) {
-    let i = 0;
-    while (i < row.length) {
-      if (row[i]) {
-        if (row[i+1]) {
-          horizWordArr.push(getHorizWord(row, i));
-          i++;
-        } else i++;
-        break;
-      } else i++;
-    }
+  let i = 0;
+  while (i < row.length) {
+  if (row[i]) {
+  if (row[i+1]) {
+  horizWordArr.push(getHorizWord(row, i));
+  i++;
+  } else i++;
+  break;
+  } else i++;
+  }
   }
   let vertWordArr = [];
   var i = 0
   while(i < arr.length) {
-    let currentRow = arr[i];
-    for (var k = 0; k < currentRow.length; k++) {
-      if (currentRow[k]){
-        let nextRow = arr[i+1];
-        let prevRow = arr[i-1];
-        if (!prevRow && nextRow[k] || !prevRow[k] && nextRow[k]) {
-          vertWordArr.push(getVertWord(arr.slice(i), k))
-        }
-      }
-    }
-    i++
+  let currentRow = arr[i];
+  for (var k = 0; k < currentRow.length; k++) {
+  if (currentRow[k]){
+  let nextRow = arr[i+1];
+  let prevRow = arr[i-1];
+  if (!prevRow && nextRow[k] || !prevRow[k] && nextRow[k]) {
+  vertWordArr.push(getVertWord(arr.slice(i), k))
+  }
+  }
+  }
+  i++
   }
   return vertWordArr.concat(horizWordArr)
-}
+  }
 
-function getVertWord(arrs, i) {
+  function getVertWord(arrs, i) {
   let tempWord = '';
   for (let row of arrs) {
-    if (row[i]) {
-      tempWord += row[i];
-    }
+  if (row[i]) {
+  tempWord += row[i];
+  }
   }
   return tempWord;
-}
+  }
 
-function getHorizWord (arr, i) {
+  function getHorizWord (arr, i) {
   let newWord = '';
   while (arr[i]) {
-    newWord+=arr[i];
-    i++;
+  newWord+=arr[i];
+  i++;
   }
   return newWord;
-}
+  }
 
-banana(board)
+  banana(board)
